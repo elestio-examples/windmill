4f0e1d96b102e4ac93eadf0531159c6689499491
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-16 14:16:53 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,5 +3,44 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-# sleep 30s;
+sleep 30s;
+
+
+token=$(curl https://${BASE_URL}/api/auth/login \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
+  --data-raw '{"email":"admin@windmill.dev","password":"changeme"}' \
+  --compressed)
+
+
+  # curl https://${BASE_URL}/api/users/setpassword \
+  # -H 'accept: application/json' \
+  # -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  # -H 'content-type: application/json' \
+  # -H 'cookie: token='${token}'; first_time=1' \
+  # -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
+  # --data-raw '{"password":"'${ADMIN_PASSWORD}'"}' \
+  # --compressed
+
+# Create new User
+  curl https://${BASE_URL}/api/users/create \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'content-type: application/json' \
+  -H 'cookie: first_time=1; token='${token}'' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
+  --data-raw '{"email":"'${ADMIN_EMAIL}'","password":"'${ADMIN_PASSWORD}'","super_admin":true,"name":"jo","company":"Jo"}' \
+  --compressed
+
+# Delete admin@windmill
+  curl https://${BASE_URL}/api/users/delete/admin@windmill.dev \
+  -X 'DELETE' \
+  -H 'accept: application/json' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cookie: first_time=1; token='${token}'' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' \
+  --compressed
+
 
